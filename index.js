3c299d0429c69235f2e0298f81443da418a943ae
Andrew Dunn <atd2005@gmail.com>
2017-08-03 12:31:20 -0400

    add framework

---
+++ index.js
@@ -0,0 +1,20 @@
+// weather endpoint
+// http://api.apixu.com/v1/current.json?key=0e65f225e29f4311bc0155702170308&q=10001
+// challenge
+// when the user pushes the weather button (id="weatherBtn")
+// the current nyc weather is printed in the div with (id="weatherInfo")
+
+// to use local server:
+// python -m SimpleHTTPServer 8000
+// python (2.7)
+//
+// or
+//
+// python3 -m http.server
+
+// you may use jquery
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    
+});
