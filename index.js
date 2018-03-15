6bcd7829dcc14820d44e9037bddcc857f235950a
Dylan McBurnett <Dylan.McBurnett@gmail.com>
2018-03-15 17:18:22 -0400

    click button to get weather

--- index.js
+++ index.js
@@ -15,6 +15,16 @@
 // you may use jquery
 
 $( document ).ready(function() {
+    $('#weatherBtn').on('click', function() {
+        $.get(
+            "http://api.apixu.com/v1/current.json?key=0e65f225e29f4311bc0155702170308&q=10001",
+            function(data) {
+                console.log(data);
+                // $(`<div>${data.current.condition.text}</div>`).appendTo('#weatherInfo'); 
+               $('#weatherInfo').html(data.current.condition.text);               
+            }
+        );
+    })
     console.log( "ready!" );
     
 });
